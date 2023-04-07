7a715cb807fe550fbb266b5082b142b53b8b2c83
Reece Williams <reecepbcups@gmail.com>
2023-04-07 10:11:38 -0500

    Adds internal capabilities + enum

--- app/app.go
+++ app/app.go
@@ -138,6 +138,10 @@ var (
 	// of "EnableAllProposals" (takes precedence over ProposalsEnabled)
 	// https://github.com/CosmWasm/wasmd/blob/02a54d33ff2c064f3539ae12d75d027d9c665f05/x/wasm/internal/types/proposal.go#L28-L34
 	EnableSpecificProposals = ""
+
+	// Features which are enabled. Blank = All.
+	// All options with `tokenfactorytypes.Enable*`
+	EnabledCapabilities = []string{}
 )
 
 // GetEnabledProposals parses the ProposalsEnabled / EnableSpecificProposals values to
@@ -462,6 +466,7 @@ func NewWasmApp(
 		app.AccountKeeper,
 		app.BankKeeper,
 		app.DistrKeeper,
+		EnabledCapabilities,
 	)
 	app.TokenFactoryKeeper = tokenFactoryKeeper
 
--- x/tokenfactory/keeper/createdenom.go
+++ x/tokenfactory/keeper/createdenom.go
@@ -82,8 +82,10 @@ func (k Keeper) chargeForCreateDenom(ctx sdk.Context, creatorAddr string, _ stri
 			return err
 		}
 	} else {
-		gasIncrease := k.GetParams(ctx).DenomCreationGasConsume
-		ctx.GasMeter().ConsumeGas(gasIncrease, "consume denom creation gas")
+		if types.IsCapabilityEnabled(k.enabledCapabilities, types.EnableGasConsumeCreateCost) {
+			gasIncrease := k.GetParams(ctx).DenomCreationGasConsume
+			ctx.GasMeter().ConsumeGas(gasIncrease, "consume denom creation gas")
+		}
 	}
 	return nil
 }
