5a5f639be3da66fe4a054668bacda6ee87e7b8b6
Reece Williams <reecepbcups@gmail.com>
2023-04-22 08:00:33 -0500

    https://github.com/osmosis-labs/osmosis/pull/4983

--- x/tokenfactory/types/capabilities.go
+++ x/tokenfactory/types/capabilities.go
@@ -1,10 +1,9 @@
 package types
 
 const (
-	EnableSetMetadata          = "enable_metadata"
-	EnableForceTransfer        = "enable_force_transfer"
-	EnableBurnFrom             = "enable_burn_from"
-	EnableGasConsumeCreateCost = "enable_gas_consume_create_cost"
+	EnableSetMetadata   = "enable_metadata"
+	EnableForceTransfer = "enable_force_transfer"
+	EnableBurnFrom      = "enable_burn_from"
 )
 
 func IsCapabilityEnabled(enabledCapabilities []string, capability string) bool {
